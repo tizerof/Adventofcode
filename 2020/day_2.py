6b294a59f57aabff47120481ed6048a717e2dfcd
Евгений <tizerofspar@yandex.ru>
2020-12-03 01:19:26 +0300

    day two solution

--- 2020/day_2.py
+++ 2020/day_2.py
@@ -1,6 +1,6 @@
 def read_passwords(file):
     """
-    Reading a file with a list of dictionaries 
+    Reading a file with a list of dictionaries
     with password policy, letter and password.
     """
     list_of_passwords = []
@@ -11,26 +11,34 @@ def read_passwords(file):
         letter = conditions[1][0]
         password = conditions[2].split("\n")[0]
         list_of_passwords.append({"password_policy": password_policy,
-                                  "letter": letter, 
+                                  "letter": letter,
                                   "password": password})
     return list_of_passwords
 
 
 def count_valid_passwords_first(list_of_passwords):
     """
-    Count the number of passwords that have a given password policy numbers of letters.
+    Count the number of passwords that have a
+    given password policy numbers of letters.
     """
-    result = [1 for line in list_of_passwords if line["password"].count(line["letter"]) in range(*line["password_policy"])]
+    result = [1 for line
+              in list_of_passwords
+              if line["password"].count(line["letter"])
+              in range(*line["password_policy"])]
     return sum(result)
 
 
 def count_valid_passwords_second(list_of_passwords):
     """
-    Count the number of passwords that have only one letter in indices of password policy.
+    Count the number of passwords that have
+    only one letter in indices of password policy.
     """
-    result = [1 for line in list_of_passwords if (line["password"][line["password_policy"][0]-1] == line["letter"])^
+    result = [1 for line
+              in list_of_passwords
+              if (line["password"][line["password_policy"][0]-1] == line["letter"]) ^
               (line["password"][line["password_policy"][1]-2] == line["letter"])]
     return sum(result)
+
 
 f = open('day_2.txt')
 list_of_passwords = read_passwords(f)
