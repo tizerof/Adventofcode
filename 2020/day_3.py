b16e75b4c60385d2f8d42180f6c3cf01a7a35c7d
Евгений <tizerofspar@yandex.ru>
2020-12-03 18:29:06 +0300

    day three solution

---
+++ 2020/day_3.py
@@ -0,0 +1,34 @@
+def count_trees_first(file_name):
+    """
+    Find the number of trees with a step
+    of 3 to the right and 1 down.
+    """
+    road = open(file_name).read().split("\n")
+    counter = 3
+    count_trees = 0
+    for line in road[1:]:
+        if line[counter] == "#":
+            count_trees +=1
+        counter = (counter + 3) % len(line)
+    return count_trees
+
+
+def count_trees_second(file_name):
+    """
+    Find multiply together the number of trees
+    encountered on each of the listed slopes.
+    """
+    road = open(file_name).read().split("\n")
+    result = 1
+    for r,d in [(1,1),(3,1),(5,1),(7,1),(1,2)]:
+        counter = r
+        count_trees = 0
+        for line in road[d::d]:
+            if line[counter] == "#":
+                count_trees +=1
+            counter = (counter + r) % len(line)
+        result *= count_trees
+    return result
+
+print(count_trees_first("day_3.txt"))
+print(count_trees_second("day_3.txt"))
